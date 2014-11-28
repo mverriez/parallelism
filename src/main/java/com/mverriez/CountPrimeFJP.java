8a39108d6d83d02540d861ee9f7ba3f3dd0a2c4d
Manuel Verriez <mverriez@yahoo.fr>
2014-11-28 10:46:20 +0100

    parallel count prime numbers with fork join pool

---
+++ src/main/java/com/mverriez/CountPrimeFJP.java
@@ -0,0 +1,64 @@
+package com.mverriez;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.IntStream;
+
+/**
+ * @author mverriez
+ */
+public class CountPrimeFJP {
+
+
+    private final ForkJoinPool forkJoinPool;
+
+    CountPrimeFJP(){
+        forkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+    }
+
+    public Integer count(Integer limit){
+        return forkJoinPool.invoke(new CountPrimeFJPTask(0, limit));
+    }
+
+
+    public class CountPrimeFJPTask extends RecursiveTask<Integer> {
+
+        private final Integer left;
+        private final Integer right;
+
+        CountPrimeFJPTask(Integer left, Integer right){
+            this.left = left;
+            this.right = right;
+        }
+
+        protected Integer compute() {
+            if (right - left < 10){
+                int count=0;
+                for (int i = left; i <= right; i++) {
+                    if (isPrime(i)){
+                        count ++;
+                    }
+                }
+                return count;
+            }else{
+                Integer middle = left + (right - left) / 2;
+                CountPrimeFJPTask task1 = new CountPrimeFJPTask(left, middle);
+                CountPrimeFJPTask task2 = new CountPrimeFJPTask(middle + 1, right);
+                task2.fork();
+                return  task1.compute() + task2.join();
+            }
+        }
+
+        public boolean isPrime(Integer n){
+/*            final String name = Thread.currentThread().getName();
+            map.compute(name, (k, v) -> v==null ? 1:v+1);*/
+            if (n % 2 == 0) return false;
+            for (int i = 3; i * i <= n; i += 2) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- src/test/java/com/mverriez/CountPrimeTest.java
+++ src/test/java/com/mverriez/CountPrimeTest.java
@@ -7,11 +7,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 
 @RunWith(BlockJUnit4ClassRunner.class)
-public class CountPrimeThreadTest extends TestCase {
+public class CountPrimeTest extends TestCase {
 
     @Test
     public void testCount() throws Exception {
         long count = new CountPrimeThread().count(1000000);
+        Assert.assertEquals(78498,count);
+    }
+
+    @Test
+    public void testCountFJP() throws Exception {
+        long count = new CountPrimeFJP().count(1000000);
         Assert.assertEquals(78498,count);
     }
 }
